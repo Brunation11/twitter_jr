5db2bcbdbfabd82926b651ce48846f720a51e917
Bruno Zatta <bzatta@gmail.com>
2016-02-02 15:52:27 -0500

    add routes for tweets, hashtags, root, and devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root 'static_pages#landing_page'
+  devise_for :users, :controllers => { registrations: 'registrations' }
   resources :tweets, except: [:new, :edit]
   resources :hashtags, except: [:new, :edit]
 
