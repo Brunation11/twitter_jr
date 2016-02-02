876b08363229236b34fc0627816fd113e65eeb76
Bruno Zatta <bzatta@gmail.com>
2016-02-02 15:50:22 -0500

    add tweets controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,8 +1,9 @@
 class TweetsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_tweet, only: [:show, :edit, :update, :destroy]
 
   def index
-    @tweets = Tweet.all
+    @tweets = current_user.tweets
   end
 
   def new
